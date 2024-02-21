1447294db437f161d2f79a567d3b95c57b71dadd
Rehyan yadav <ritulyadav1984@gmail.com>
2024-02-21 22:06:20 +0530

    functions

---
+++ function/questions3.cpp
@@ -0,0 +1,22 @@
+//swap two numbers
+
+
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+
+
+int x =2;
+int y =3;
+
+cout<<x<<" "<<y<<endl;
+ x = x+y;
+ y  = x-y;
+ x =x-y;
+ cout<<x<<" "<<y<<endl;
+
+    return 0;
+}
--- function/scope.cpp
+++ function/scope.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
+
+//This is the global variables 
+int global =5;
+
+
+//main functions 
+
 int main()
+
 {
+
+    //examples of local variables 
+    //this is local variables 
+    int global =2;
+    cout<<global<<endl;
     return 0;
 }
+/*
+There are mainly two types of scopes
+1) globle scopes
+>They are available through out the life time of program 
+>They declare at the top of the program outside all of the functions or blocks 
+>Declaring Global variables : Globla Variables are usually 
+declared outside the of the program.they can access any portion of the program 
+
+
+
+2)Local Scopes 
+>Anything inside the {  and } to inside the block.
+>Local variables do not exist outside the block in which they are declared
+>declaring local variables are inside the block
+*/
