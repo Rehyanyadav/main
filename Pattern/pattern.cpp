8146743067b652a3668890d48b8bf300d81c3859
root:$rehyan_yadav <67955608+Rehyanyadav@users.noreply.github.com>
2024-04-06 01:06:43 +0530

    practicing pattern problems

---
+++ Pattern/pattern.cpp
@@ -0,0 +1,91 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //pattern
+
+/*
+1111
+2222
+3333
+4444
+
+
+int n;
+cin >>n ;
+int i =1 ;
+while(i<= n){
+
+ int j =1; 
+while(j <=n){
+     cout <<i;
+     j++;
+}
+cout<<endl;
+i++;
+
+
+
+}
+*/
+
+/*
+1234
+1234
+1234
+1234
+
+int n;
+cin >>n ;
+int i =1 ;
+while(i<= n){
+
+ int j =1; 
+while(j <=n){
+     cout <<j;
+     j++;
+}
+cout<<endl;
+i++;
+
+
+
+}
+*/
+
+/*
+4321
+4321
+4321
+4321
+
+int n;
+cin >>n ;
+int i =1 ;
+while(i<= n){
+
+ int j =1; 
+while(j <=n){
+     cout <<n-j+1;
+     j++;
+  
+
+
+
+}
+cout<<endl;
+i++;
+
+
+
+}
+*/
+
+
+
+
+/// code  for all the pattern is given
+
+}
