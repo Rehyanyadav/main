ed510f73abbdd96599edb455c2f38a9095ae41a8
root:$rehyan_yadav <ritulyadav1984@gmail.com>
2024-08-09 10:01:25 +0530

    today's commit

---
+++ c_2330104/Structures/chap04.cpp
@@ -0,0 +1,59 @@
+/*
+
+WAP  in  c using structures to store n employee’s data such as employee name, gender, designation,
+department, basic pay. Calculate the gross pay of each employees as follows:
+Gross pay = basic pay + HR + DA
+HR=25% of basic and DA=75% of basic. 
+*/
+#include <stdio.h>
+
+struct Employee {
+    char name[50];
+    char gender;
+    char designation[50];
+    char department[50];
+    float basicPay;
+    float grossPay;
+};
+
+void calculateGrossPay(struct Employee* emp) {
+    emp->grossPay = emp->basicPay + (0.25 * emp->basicPay) + (0.75 * emp->basicPay);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+
+    struct Employee employees[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for Employee %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%c", employees[i].name);
+        printf("Gender: ");
+        scanf(" %c", &employees[i].gender);
+        printf("Designation: ");
+        scanf("%c", employees[i].designation);
+        printf("Department: ");
+        scanf("%c", employees[i].department);
+        printf("Basic Pay: ");
+        scanf("%f", &employees[i].basicPay);
+
+        calculateGrossPay(&employees[i]);
+    }
+
+    printf("\nEmployee Details:\n");
+    for (int i = 0; i < n; i++) {
+        printf("\nEmployee %d:\n", i + 1);
+        printf("Name: %s\n", employees[i].name);
+        printf("Gender: %c\n", employees[i].gender);
+        printf("Designation: %s\n", employees[i].designation);
+        printf("Department: %s\n", employees[i].department);
+        printf("Basic Pay: %.2f\n", employees[i].basicPay);
+        printf("Gross Pay: %.2f\n", employees[i].grossPay);
+    }
+
+    return 0;
+}
+
